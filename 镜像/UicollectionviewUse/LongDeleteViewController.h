59f4e3bef6192283d05419d0241bd8ac5e480f9c
dorayo <yomingyo@gmail.com>
2016-06-27 20:30:08 +0800

    .

---
+++ 镜像/UicollectionviewUse/LongDeleteViewController.h
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.h
+//  UICollectionview
+//
+//  Created by Xia_Q on 15/11/12.
+//  Copyright (c) 2015年 XiaQiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CollectionViewCell.h"
+
+@interface LongDeleteViewController : UIViewController
+
+@property(nonatomic,strong)UICollectionView *collectionView;
+
+@end
